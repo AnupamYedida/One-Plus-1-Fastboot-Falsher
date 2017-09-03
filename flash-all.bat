706f96da4b84456a92a98dd7101dac076d8457a0
AnupamYedida <anupamy@gmail.com>
2017-09-03 10:50:45 +0530

    Add files via upload

---
+++ flash-all.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+
+PATH=%PATH%;"%SYSTEMROOT%\System32"
+fastboot flash modem NON-HLOS.bin
+fastboot flash sbl1 sbl1.mbn
+fastboot flash dbi sdi.mbn
+fastboot flash aboot emmc_appsboot.mbn
+fastboot flash rpm rpm.mbn
+fastboot flash tz tz.mbn
+fastboot flash LOGO logo.bin
+fastboot flash oppostanvbk static_nvbk.bin
+fastboot flash recovery recovery.img
+fastboot flash system system.img
+fastboot flash boot boot.img
+fastboot flash cache cache.img
+fastboot flash userdata userdata_64G.img
+fastboot reboot
+
+echo Press any key to exit...
+pause >nul
+exit
